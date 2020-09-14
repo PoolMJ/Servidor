55621376b4f10a0353aabcc405f15d50c753ed52
Paul Maldonado <maldonadopaul92@gmail.com>
2020-09-14 02:31:18 -0500

    crrcn3

--- static/js/index.js
+++ static/js/index.js
@@ -3,10 +3,20 @@
 
 
 
+let timeNow = new Date();
+    
+// Queremos que la hora se muestre siempre con 2 dígitos. Para eso, hacemos lo siguiente:
+// Usamos un ternario para saber si el número de digitos es menor que 2
+let hours = timeNow.getHours().toString().length < 2 ? "0" + timeNow.getHours() : timeNow.getHours();
+let minutes = timeNow.getMinutes().toString().length < 2 ? "0" + timeNow.getMinutes() : timeNow.getMinutes();
+let seconds = timeNow.getSeconds().toString().length < 2 ? "0" + timeNow.getSeconds() : timeNow.getSeconds();
 
+//  Concatenando variables | Usando ES5 
+// let mainTime = hours + ":" + minutes + ":" + seconds;
+ //  Concatenando variables | Usando ES6: Template Strings (Template literals) 
+let mainTime = `${hours}:${minutes}:${seconds}`;
 
-
-
+console.log(mainTime);
 
 
 
