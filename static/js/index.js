c0d2b802b87d6fd1f35cd7822a7d8bf7953370a7
Paul Maldonado <maldonadopaul92@gmail.com>
2020-09-14 00:09:54 -0500

    c12

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,55 @@
 //https://www.eclipse.org/paho/clients/js/
 
 
-function total() {
-	var total;
-	t="horaactual";
-	message = new Paho.MQTT.Message(""+" "+ t +" "+ y );
-    message.destinationName = "pemaldonado.fie@unach.edu.ec/RASP";
-    client.send(message);
+
+(function(){
+	var actualizarHora = function(){
+		var fecha = new Date(),
+			horas = fecha.getHours(),
+			ampm,
+			minutos = fecha.getSeconds(),
+			segundos = fecha.getDay(),
+		
+		var pHoras = document.getElementById('horas');
+			pAMPM = document.getElementById('ampm');
+			pMinutos = document.getElementById('minutos');
+			pSegundos = document.getElementById('segundos');
+		
+		if (horas >= 12) {
+			horas = horas - 12;
+			ampm = 'PM';
+		} else {
+			ampm = 'AM';
+		}
+		
+		if (horas == 0){
+			horas = 12;
+		};
+		
+		
+		pHoras.textContent = horas;
+		pAMPM.textContent = ampm;
+		
+		if (minutos < 10){minutos = "0" + minutos};
+		if (segundos < 10){segundos = "0" + segundos};
+		
+		
+		pMinutos.textContent = minutos;
+		pSegundos.textContent = segundos;
+		
+		
+	};	
+		
+		
+		
 	
-}
+	
+	
+	actualizarHora();
+	
+}())
+
+
 
 
 // Create a client instance
