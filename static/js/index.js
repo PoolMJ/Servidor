fed8d7e1a0ba2bfb92cbb9c563d1edf976fcf610
Paul Maldonado <maldonadopaul92@gmail.com>
2020-09-19 20:55:25 -0500

    aaaa1

--- static/js/index.js
+++ static/js/index.js
@@ -3,24 +3,6 @@
 
 
 
-let timeNow = new Date();
-    
-// Queremos que la hora se muestre siempre con 2 dígitos. Para eso, hacemos lo siguiente:
-// Usamos un ternario para saber si el número de digitos es menor que 2
-let hours = timeNow.getHours().toString().length < 2 ? "0" + timeNow.getHours() : timeNow.getHours();
-let minutes = timeNow.getMinutes().toString().length < 2 ? "0" + timeNow.getMinutes() : timeNow.getMinutes();
-let seconds = timeNow.getSeconds().toString().length < 2 ? "0" + timeNow.getSeconds() : timeNow.getSeconds();
-
-//  Concatenando variables | Usando ES5 
-// let mainTime = hours + ":" + minutes + ":" + seconds;
- //  Concatenando variables | Usando ES6: Template Strings (Template literals) 
-let mainTime = `${hours}:${minutes}:${seconds}`;
-
-console.log(mainTime);
-
-
-
-
 
 
 // Create a client instance
@@ -47,9 +29,9 @@ console.log(mainTime);
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("pemaldonado.fie@unach.edu.ec/WEB");
+    client.subscribe("pemaldonado.fie@unach.edu.ec/pagweb");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "pemaldonado.fie@unach.edu.ec/RASP";
+    message.destinationName = "pemaldonado.fie@unach.edu.ec/psd";
     client.send(message);
 	
   }
@@ -66,9 +48,19 @@ console.log(mainTime);
     }
   }
 
+
+
+
+
   // called when a message arrives
   function onMessageArrived(message) {
-     text=(message.payloadString);
-	 console.log(text)
-	 document.getElementById("respuesta").innerHTML = text;
+     identificador=(message.payloadString).split(" ")[0];
+	 dato=(message.payloadString).split(" ")[1];
+	 console.log("hora recibida")
+	 if(identificador=="H")
+		 document.getElementById("hora").innerHTML = dato;
+	 if(identificador=="M")
+		 document.getElementById("minuto").innerHTML = dato;
+	 if(identificador=="S")
+		 document.getElementById("segundo").innerHTML = dato;
   }
