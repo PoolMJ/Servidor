bdf326ecf4a22351f628e1daf6927580d3dcd127
Paul Maldonado <maldonadopaul92@gmail.com>
2020-09-20 19:32:53 -0500

    aaaa6

--- static/js/index.js
+++ static/js/index.js
@@ -56,13 +56,17 @@
 
 
 function LED_PRENDIDO_1(){
+	console.log("Led Prendido")
 	massage=new Paho.MQTT.Message("led1p")
 	message.destinationName="pemaldonado.fie@unach.edu.ec/psd";
+	client.send(message);
 }
 
 function LED_APAGADO_2(){
+	console.log("Led Prendido")
 	massage=new Paho.MQTT.Message("led1a")
 	message.destinationName="pemaldonado.fie@unach.edu.ec/psd";
+	client.send(message);
 }
 
 
