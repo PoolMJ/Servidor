25699dea31caba12e5606a35a6cd3f7038ec370e
Paul Maldonado <maldonadopaul92@gmail.com>
2020-09-21 19:35:24 -0500

    srmd20

--- static/js/index.js
+++ static/js/index.js
@@ -89,11 +89,16 @@ function DIVISION(){
 	n1=document.getElementById("n1").value;
 	n2=document.getElementById("n2").value;
 	message = new Paho.MQTT.Message("DV"+" "+ n1 +" "+ n2 );
-	message = new Paho.MQTT.Message("E"+" "+ n1 +" "+ n2 );
 	message.destinationName="pemaldonado.fie@unach.edu.ec/psd";
 	client.send(message);
 }
 
-
+function DIVISION(){
+	n1=document.getElementById("n1").value;
+	n2=document.getElementById("n2").value;
+	message = new Paho.MQTT.Message("E"+" "+ n1 +" "+ n2 );
+	message.destinationName="pemaldonado.fie@unach.edu.ec/psd";
+	client.send(message);
+}
 
 
